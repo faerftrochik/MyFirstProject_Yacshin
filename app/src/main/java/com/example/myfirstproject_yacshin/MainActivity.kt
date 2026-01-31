fb3c950157687adee3033ee27f2ab2d1b93f0a30
Souls Rider <faerftrochik@gmail.com>
2026-01-31 15:01:54 +0300

    3 Cube Nachalo №3

--- app/src/main/java/com/example/myfirstproject_yacshin/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject_yacshin/MainActivity.kt
@@ -1,32 +1,21 @@
-package com.example.myfirstproject_yacshin
+package com.example.yacshin.app
 
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import javax.annotation.processing.Generated
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    val textView_zero = findViewById<TextView>(R.id.textView_zero)
-    val randomInt = Random.nextInt(6) + 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
-            val button: Button = findViewById(R.id.button_player)
-            button.setOnClickListener ({ view -> /* do something*/ })
+        val textView = findViewById<TextView>(R.id.textView_result)
+        val button = findViewById<Button>(R.id.button_roll)
 
-
+        button.setOnClickListener {
+            val number = Random.nextInt(1, 7)
+            textView.text = "$number"
         }
     }
 }
